7aa2d46e0636b760e0ba39923e0766d1148c5430
Bekzat Gazezov <beka.g0226@gmail.com>
2024-10-01 16:06:54 +0500

    first commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,15 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ReportsModule } from './reports/reports.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/users.entity';
+import { ReportEntity } from './reports/report.entity';
+
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'sqlite',
       database: 'database.db',
-      entities: ['dist/**/*.entity.js'],
+      entities: [User, ReportEntity],
       synchronize: true,
     }),
     UsersModule,
